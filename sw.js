5e5526d5dce9363d3e62979f3d97ad5cd61bae88
Saurabh Mathur <saurabhmathur96@gmail.com>
2016-04-22 22:57:03 +0530

    cache fix

--- sw.js
+++ sw.js
@@ -26,6 +26,23 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = [CACHE_NAME];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
